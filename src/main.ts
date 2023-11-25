1a95a007fa6f07868f63f9991681a759d2315f07
bagusgandhi <bagusgandhi4@gmail.com>
2023-11-25 16:10:10 +0700

    feat: adding ValidationPIpe and setup swagger

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true
+  }));
+  app.enableCors();
+
+
+  const config = new DocumentBuilder()
+    .setTitle('Crypto Ewallet API')
+    .setDescription('Crypto Ewallet API documentations')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  Logger.debug(`${process.env.PORT}`)
+  
+  await app.listen(3000);
+}
+bootstrap();
