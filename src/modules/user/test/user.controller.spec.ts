b7aa90efb4f458326e6ab2fc683ce1b7cfe446ab
bagusgandhi <bagusgandhi4@gmail.com>
2023-11-29 08:00:21 +0700

    fix: handling expect negative test case on "User already exists"

--- src/modules/user/test/user.controller.spec.ts
+++ src/modules/user/test/user.controller.spec.ts
@@ -3,10 +3,11 @@ import { UserController } from "../user.controller";
 import { JwtStrategy } from "../../auth/jwt/jwt.strategy";
 import { RegisterDto } from "../dto/register.dto";
 import { JwtService } from "@nestjs/jwt";
-import { ConflictException, Logger } from "@nestjs/common";
+import { ConflictException, HttpException, HttpStatus, Logger } from "@nestjs/common";
 import { UserService } from "../user.service";
 import { PrismaService } from "../../../common/database/prisma.service";
 import { v4 as uuidv4 } from 'uuid';
+import { TransactionsService } from "../../../modules/transactions/transactions.service";
 
 describe('UserController', () => {
     let userController: UserController;
@@ -15,7 +16,7 @@ describe('UserController', () => {
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             controllers: [UserController],
-            providers: [JwtStrategy, JwtService, UserService, PrismaService],
+            providers: [JwtStrategy, JwtService, UserService, PrismaService, TransactionsService],
         }).compile();
 
         userController = module.get<UserController>(UserController);
@@ -24,25 +25,31 @@ describe('UserController', () => {
 
     describe('registerUser', () => {
         it('should register new user and return the token', async () => {
-            const registerDto: RegisterDto = { username: 'usertest1129' }
+            const registerDto: RegisterDto = { username: 'existingUser' }
             const expectedToken = 'generated_token';
             const id = uuidv4();
 
-            jest.spyOn(userService, 'registerUser').mockResolvedValue({ id, username: 'existingUser', balance: 0, timestamp: new Date(), token: expectedToken });
+            jest.spyOn(userService, 'registerUser').mockResolvedValue({ id, username: 'existingUser', balance: 0, timestamp: new Date('2023-11-29T00:34:31.675Z'), token: expectedToken });
 
             const result = await userController.register(registerDto);
             Logger.debug(result);
 
-            expect(result).toEqual({ token: expectedToken });
+            expect(result).toEqual({ id, username: 'existingUser', balance: 0, timestamp: new Date('2023-11-29T00:34:31.675Z'), token: expectedToken });
         })
 
-        it('should throw error user lreasy exist is user exists', async () => {
+        it('should throw error user alreasy exist if user exists', async () => {
             const registerDto: RegisterDto = { username: 'existingUser' }
             const id = uuidv4();
 
-            jest.spyOn(userService, 'findByUsername').mockResolvedValue({id, username: 'existingUser', balance: 0, timestamp: new Date() });
+            jest.spyOn(userService, 'findByUsername').mockResolvedValue({ id, username: 'existingUser', balance: 0, timestamp: new Date() });
 
-            await expect(userService.registerUser(registerDto)).rejects.toThrow(ConflictException);
+            try {
+                await userController.register(registerDto);
+            } catch (error) {
+                expect(error).toBeInstanceOf(HttpException);
+                expect(error.message).toEqual('Username already exists');
+                expect(error.getStatus()).toEqual(HttpStatus.CONFLICT);
+            }
         })
     })
 })
