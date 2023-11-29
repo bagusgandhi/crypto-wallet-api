fc9e7a1e9a353eaf9f4e06dd099b8990a9fdb658
bagusgandhi <bagusgandhi4@gmail.com>
2023-11-29 09:42:21 +0700

    feat: adding password and fullname when register

--- src/modules/user/dto/register.dto.ts
+++ src/modules/user/dto/register.dto.ts
@@ -1,9 +1,28 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsString } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class RegisterDto {
-    @ApiProperty({ example: "johndoe" })
+    @ApiProperty({
+        description: 'Full Name',
+        example: 'John Doe',
+    })
+    @IsNotEmpty()
+    @IsString()
+    full_name: string;
+
+    @ApiProperty({
+        description: 'Username',
+        example: 'johndoe',
+    })
     @IsNotEmpty()
     @IsString()
     username: string;
+
+    @ApiProperty({
+        description: 'Password',
+        example: 'secret321okay',
+    })
+    @IsNotEmpty()
+    @IsString()
+    password: string;
 }
