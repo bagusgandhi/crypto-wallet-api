74f558cae654c128ae64b7813a6178afc1f4ff3b
bagusgandhi <bagusgandhi4@gmail.com>
2023-11-29 23:42:53 +0700

    feat: create dto query transaction log

---
+++ src/modules/transactions/dto/query-log.dto.ts
@@ -0,0 +1,32 @@
+import { IsEnum, IsNumber, IsString, IsOptional } from "class-validator";
+import { TransactionType } from "../../../common/enum/transaction-type.enum";
+import { Transform } from "class-transformer";
+import { toNumber } from "src/common/utils/cast.helper";
+
+export class QueryTransactionLogDto {
+    @IsString()
+    @IsOptional()
+    user_id:string;
+
+    @IsString()
+    @IsOptional()
+    from: string;
+
+    @IsString()
+    @IsOptional()
+    to: string;
+
+    @IsEnum(TransactionType)
+    @IsOptional()
+    transaction_type: string;
+
+    @Transform(({ value }) => toNumber(value, { default: 1, min: 1 }))
+    @IsNumber()
+    @IsOptional()
+    limit: number;
+
+    @Transform(({ value }) => toNumber(value, { default: 1, min: 1 }))
+    @IsNumber()
+    @IsOptional()
+    page: number;
+}
