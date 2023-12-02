d688060cbcda6827e809bbd7825f9128e6dea965
bagusgandhi <bagusgandhi4@gmail.com>
2023-12-02 11:24:44 +0700

    feat: register cache module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,21 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
 import { TransactionsModule } from './modules/transactions/transactions.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
+import { Env } from './common/config/env-loader';
 
+const { REDIS_HOST, REDIS_PORT  } = Env()
 @Module({
-  imports: [AuthModule, UserModule, TransactionsModule],
+  imports: [
+    CacheModule.register({ 
+      isGlobal: true,
+      host: REDIS_HOST,
+      port: REDIS_PORT,
+      store: redisStore
+    }), 
+    AuthModule, 
+    UserModule, 
+    TransactionsModule],
 })
 export class AppModule { }
