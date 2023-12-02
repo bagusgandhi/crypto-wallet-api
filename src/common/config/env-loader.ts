1fe8bf131cfecf19ed63b90c6ee5db980717b5d1
bagusgandhi <bagusgandhi4@gmail.com>
2023-12-02 11:25:26 +0700

    feat: add env Redis

--- src/common/config/env-loader.ts
+++ src/common/config/env-loader.ts
@@ -2,5 +2,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const Env = () => ({ 
-    JWT_SECRET: process.env.JWT_SECRET
+    JWT_SECRET: process.env.JWT_SECRET,
+    REDIS_HOST: process.env.REDIS_HOST,
+    REDIS_PORT: process.env.REDIS_PORT
 });
