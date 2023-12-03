4683a840e4059382ef6406d113ed4a5beb60bb8b
bagusgandhi <bagusgandhi4@gmail.com>
2023-12-03 07:44:03 +0700

    feat: remove import PrismaModule

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,6 +1,5 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { UserService } from './user.service';
-import { PrismaModule } from '../../common/database/prisma.module';
 import { UserController } from './user.controller';
 import { AuthModule } from '../auth/auth.module';
 import { TransactionsModule } from '../transactions/transactions.module';
@@ -8,7 +7,6 @@ import { TransactionsModule } from '../transactions/transactions.module';
 @Module({
   imports: [
     forwardRef(() => AuthModule),
-    PrismaModule,
     TransactionsModule,
   ],
   providers: [UserService],
