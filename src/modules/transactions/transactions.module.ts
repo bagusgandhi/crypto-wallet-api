0c68ab774dc16e624e3327bf66e77edf11b98dbd
bagusgandhi <bagusgandhi4@gmail.com>
2023-12-03 07:42:09 +0700

    feat: remove import PrismaModule

--- src/modules/transactions/transactions.module.ts
+++ src/modules/transactions/transactions.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
-import { PrismaModule } from '../../common/database/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
   providers: [TransactionsService],
   controllers: [TransactionsController],
   exports: [TransactionsService]
