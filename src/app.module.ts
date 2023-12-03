1b94b52cb549280caec3852ded33e4243022cce5
bagusgandhi <bagusgandhi4@gmail.com>
2023-12-03 07:40:54 +0700

    feat: adding prisma module and prisma filter exception

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,33 @@ import { AuthModule } from './modules/auth/auth.module';
 import { CacheModule } from '@nestjs/cache-manager';
 import * as redisStore from 'cache-manager-redis-store';
 import { Env } from './common/config/env-loader';
+import { PrismaClientExceptionFilter, PrismaModule } from 'nestjs-prisma';
+import { APP_FILTER, HttpAdapterHost } from '@nestjs/core';
 
-const { REDIS_HOST, REDIS_PORT  } = Env()
+const { REDIS_HOST, REDIS_PORT } = Env()
 @Module({
   imports: [
-    CacheModule.register({ 
+    PrismaModule.forRoot({
+      isGlobal: true,
+    }),
+    CacheModule.register({
       isGlobal: true,
       host: REDIS_HOST,
       port: REDIS_PORT,
       store: redisStore
-    }), 
-    AuthModule, 
-    UserModule, 
-    TransactionsModule],
+    }),
+    AuthModule,
+    UserModule,
+    TransactionsModule
+  ],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useFactory: ({ httpAdapter }: HttpAdapterHost) => {
+        return new PrismaClientExceptionFilter(httpAdapter);
+      },
+      inject: [HttpAdapterHost],
+    },
+  ]
 })
 export class AppModule { }
